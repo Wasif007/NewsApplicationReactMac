9e3c749a9d61249c7d864fb67d98281024b52cee
Wasif007 <wasifateeq007@hotmail.com>
2023-07-04 22:42:41 +0500

    Refracting of Code

--- src/App.js
+++ src/App.js
@@ -1,39 +1,117 @@
+import "./App.css";
 
-import './App.css';
-
-import React, { Component } from 'react'
-import Navbar from './Components/Navbar';
-import News from './Components/News';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import React, { Component } from "react";
+import Navbar from "./Components/Navbar";
+import News from "./Components/News";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 export class App extends Component {
   render() {
     return (
       <div>
         <Router>
-      <Navbar/>
-      <Routes>
-      <Route path='/'   
-element={<News exact key="general" pageSize={10} country="in" category="general"/>} /> 
-      <Route path='/business' 
-element={<News  exact key="business"pageSize={10} country="in" category="business"/>} /> 
-<Route path='/entertainment'  
-element={<News exact key="entertainment" pageSize={10} country="in" category="entertainment"/>} /> 
-<Route path='/general'  
-element={<News exact key="general" pageSize={10} country="in" category="general"/>} /> 
-<Route path='/health'   
-element={<News exact key="health" pageSize={10} country="in" category="health"/>} /> 
-<Route path='/science'  
-element={<News exact key="science" pageSize={10} country="in" category="science"/>} /> 
-<Route path='/sports'  
-element={<News exact key="sports" pageSize={10} country="in" category="sports"/>} /> 
-<Route path='/technology'  
-element={<News exact key="technology" pageSize={10} country="in" category="technology"/>} />  
-    		</Routes>
-      </Router>
+          <Navbar />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <News
+                  exact
+                  key="general"
+                  pageSize={10}
+                  country="in"
+                  category="general"
+                />
+              }
+            />
+            <Route
+              path="/business"
+              element={
+                <News
+                  exact
+                  key="business"
+                  pageSize={10}
+                  country="in"
+                  category="business"
+                />
+              }
+            />
+            <Route
+              path="/entertainment"
+              element={
+                <News
+                  exact
+                  key="entertainment"
+                  pageSize={10}
+                  country="in"
+                  category="entertainment"
+                />
+              }
+            />
+            <Route
+              path="/general"
+              element={
+                <News
+                  exact
+                  key="general"
+                  pageSize={10}
+                  country="in"
+                  category="general"
+                />
+              }
+            />
+            <Route
+              path="/health"
+              element={
+                <News
+                  exact
+                  key="health"
+                  pageSize={10}
+                  country="in"
+                  category="health"
+                />
+              }
+            />
+            <Route
+              path="/science"
+              element={
+                <News
+                  exact
+                  key="science"
+                  pageSize={10}
+                  country="in"
+                  category="science"
+                />
+              }
+            />
+            <Route
+              path="/sports"
+              element={
+                <News
+                  exact
+                  key="sports"
+                  pageSize={10}
+                  country="in"
+                  category="sports"
+                />
+              }
+            />
+            <Route
+              path="/technology"
+              element={
+                <News
+                  exact
+                  key="technology"
+                  pageSize={10}
+                  country="in"
+                  category="technology"
+                />
+              }
+            />
+          </Routes>
+        </Router>
       </div>
-    )
+    );
   }
 }
 
-export default App
-
+export default App;
--- src/Components/Spinner.js
+++ src/Components/Spinner.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react'
-import loader from './loader.gif'
+import React, { Component } from "react";
+import loader from "./loader.gif";
 export class Spinner extends Component {
   render() {
     return (
-      <div className='text-center'>
+      <div className="text-center">
         <img src={loader} alt="loader" />
       </div>
-    )
+    );
   }
 }
-export default Spinner
+export default Spinner;
